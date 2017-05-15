3f7c5916fdb565da251bbdf964a3ce0e93f53379
1337exo <menard.ludovic@outlook.fr>
2017-05-15 13:48:06 +0200

    Doxygen (Close #5)

--- HelloWorld/src/HelloWorld.cpp
+++ HelloWorld/src/HelloWorld.cpp
@@ -1,6 +1,18 @@
+/*!
+ * \file HelloWorld.cpp
+ * \brief just un Hello world
+ * \author Ludovic Menard
+ * \version 0.1
+ */
 #include <iostream>
 
 int main(){
+      /*!
+     *  \brief Main HelloWorld
+     *
+     *  main qui affiche Hello World !! dans la console
+     *
+     */
   std::cout<<"Hello World !!"<<std::endl;
   return 0;
 }
