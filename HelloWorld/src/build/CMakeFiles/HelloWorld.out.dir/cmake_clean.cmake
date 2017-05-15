f3f14d2c92cf5820a01858951cc88060f1da1cd8
simpau <simon.ansel@outlook.com>
2017-05-15 10:22:58 +0200

    add Hello World

---
+++ HelloWorld/src/build/CMakeFiles/HelloWorld.out.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloWorld.out.dir/src/HelloWorld.cpp.o"
+  "HelloWorld.out.pdb"
+  "HelloWorld.out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloWorld.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
